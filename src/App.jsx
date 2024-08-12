fa6eb0895e34075e79a31d18ffe04200b27f7c92
Kushwanth P <Kushwanth.P@Cognizant.com>
2024-08-12 02:26:50 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+export default function App() {
+  return (
+    <div>
+    <h1 className="text-6xl font-epilogue font-bold">
+      Hello world!
+    </h1>
+    <p className="font-jura">
+      Hello Kushwanth
+    </p>
+    </div>
+  )
+}
