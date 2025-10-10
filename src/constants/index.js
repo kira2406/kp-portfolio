c7e2021d05ae0822bdb9eac183fb6dfd2dd8e133
kira2406 <kush.p030.24@gmail.com>
2025-10-09 20:20:24 -0400

    updated real estate listing

--- src/constants/index.js
+++ src/constants/index.js
@@ -231,6 +231,35 @@ export const skills = [
 
 export const projects = [
     {
+        title: "Real Estate Listing Website",
+        desc: ["A full-stack web application that allows users to create, browse and manage property listings for sale or rent. The platform supports multiple image uploads, detailed property information (bedrooms, bathrooms, price, address, amenities) and special offers. Users can filter listings based on criteria like property type, price range, and features such as furnished spaces or parking availability."],
+        deployedLink: "https://mern-estate-frontend-9m37.onrender.com/",
+        githubLink: "https://github.com/kira2406/MERN-Estate",
+        tags: ["FS", "ALL"],
+        tech: [
+        "https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white",
+        "https://img.shields.io/badge/Express.js-000000?style=for-the-badge&logo=express&logoColor=white",
+        "https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white",
+        "https://img.shields.io/badge/Firebase-FFCA28?style=for-the-badge&logo=firebase&logoColor=black",
+        "https://img.shields.io/badge/Docker-2CA5E0?style=for-the-badge&logo=docker&logoColor=white",
+        "https://img.shields.io/badge/AWS-232F3E?style=for-the-badge&logo=amazon-aws&logoColor=white",
+        "https://img.shields.io/badge/Terraform-7B42BC?style=for-the-badge&logo=terraform&logoColor=white",
+        "https://img.shields.io/badge/Redis-DC382D?style=for-the-badge&logo=redis&logoColor=white",
+        "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+        "https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white",
+        "https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white",
+        "https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E",
+        "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white",
+        "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white",
+        "https://img.shields.io/badge/Vite-646CFF?style=for-the-badge&logo=vite&logoColor=white",
+        "https://img.shields.io/badge/Tailwind_CSS-06B6D4?style=for-the-badge&logo=tailwind-css&logoColor=white",
+        "https://img.shields.io/badge/Material_UI-0081CB?style=for-the-badge&logo=mui&logoColor=white",
+        "https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white",
+        "https://img.shields.io/badge/Cloudify-000000?style=for-the-badge&logo=cloudify&logoColor=white"
+        ],
+        thumbnail: null
+    },
+    {
         title: "Kratos Digital Store",
         desc: ["Designed and developed an e-commerce website tailored for the sale of PC games.",
             "Developed an automated sales forecasting system using LSTM models, saved the model weights on AWS S3 buckets.",
